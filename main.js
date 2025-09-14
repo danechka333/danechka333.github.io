951189cd2274524c0825c605ea44ac34567dd40e
danechka333 <den10yehim@gmail.com>
2025-09-14 19:49:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const deadline = new Date('2026-02-08T06:00:00');
   
+  const elWeeks = document.querySelector('.weeks')
   const elDays = document.querySelector('.days');
   const elHours = document.querySelector('.hours');
   const elMinutes = document.querySelector('.minutes');
@@ -10,11 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const now = new Date();
     const diff = Math.max(0, deadline - now);
 
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const weeks = Math.floor(diff / (1000* 60 * 60 * 24 * 7));
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24) % 7);
     const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
     const minutes = Math.floor((diff / (1000 * 60)) % 60);
     const seconds = Math.floor((diff / 1000) % 60);
-
+    
+    elWeeks.textContent = String(weeks).padStart(2, '0');
     elDays.textContent = String(days).padStart(2, '0');
     elHours.textContent = String(hours).padStart(2, '0');
     elMinutes.textContent = String(minutes).padStart(2, '0');
